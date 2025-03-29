eb9fc299d4259be86609d474f7f7626968fcb0be
holaholu <olaoluhimself@yahoo.com>
2025-03-29 08:25:43 -0500

    Fix build issues: Update useD3 hook and CI configuration

--- src/hooks/useD3.ts
+++ src/hooks/useD3.ts
@@ -1,14 +1,14 @@
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-const useD3 = (renderChartFn: (svg: d3.Selection<SVGSVGElement, unknown, null, undefined>) => void, deps: any[]) => {
+const useD3 = (renderChartFn: (svg: d3.Selection<SVGSVGElement, unknown, null, undefined>) => void, dependencies: React.DependencyList = []) => {
   const ref = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     if (ref.current) {
       renderChartFn(d3.select(ref.current));
     }
-  }, deps);
+  }, dependencies);
 
   return ref;
 };
