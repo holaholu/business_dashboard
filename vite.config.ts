a93c0e7216126aa4437643f96084e9060d59c1f0
holaholu <olaoluhimself@yahoo.com>
2025-03-29 07:55:49 -0500

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/data-viz/', // Replace with your repository name
+  base: '/business_dashboard/', // Replace with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
